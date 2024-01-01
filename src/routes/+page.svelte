5fa19e109dfa1f588f0d07d2962b68dfdeef3917
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-01-01 17:00:54 -0300

    Setup website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import portalOmegaBanner from "../assets/portal-omega-banner.png"
+</script>
+
+<img src={portalOmegaBanner} height="300" alt="Portal Omega Banner" />
+
+<p>
+	Hello! We are a team of 13 people called Cool People Games, developing a portal mod called Portal
+	Omega. This website clearly isnt finished yet, but you can still check out our discord server,
+	apply as a test subject, and watch the mod trailer!
+</p>
+<h6>
+	the trailer is of a very early build so it doesn't look the best and our mod is way farther in
+	development and more polished currently
+</h6>
+
+<hr />
+<h3>links</h3>
+
+<a href="https://discord.gg/ystbKHKsVv" target="_blank"> Portal Omega Discord Server </a>
+<br />
+<a href="https://youtu.be/-tsmZCnG9t8?si=aHvEdeCDNrd8NFbv" target="_blank">
+	Portal Omega Trailer
+</a>
+<br />
+<a href="https://forms.gle/jsMNdzYEuiY9mY2K9" target="_blank"> Test Subject Application Form </a>
+<h6>website by cheesechair and gamesrevolution</h6>
